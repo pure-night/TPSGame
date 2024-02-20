4be591d036d4e18bc6038536f3afb312cf7bd520
pure-night <58593218+pure-night@users.noreply.github.com>
2024-02-20 20:28:39 +0900

    [Feat] Manager Base
    GameManager, UIManager의 기본적인 구조

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : GenericSingleton<GameManager>
 {
-    // Start is called before the first frame update
-    void Start()
+    public UIManager UIManager { get; private set; }
+    public SceneLoader SceneLoader { get; private set; } 
+    
+    // 게임 시작시 실행을 위한 코드
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Execute()
     {
-        
+        // GameManager의 Instance초기화를 시켜주며 Initialize로 다른 요소 초기화
+        Instance.Initialize();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Initialize()
     {
+        // UIManager 할당 or 생성
+        UIManager = TryGetComponent<UIManager>(out var uiManager) ?
+            uiManager : gameObject.AddComponent<UIManager>();
+        // UIManager의 Instance와 요소 초기화
+        UIManager.Instance.Initialize();
         
+        // SceneLoader 할당 or 생성
+        SceneLoader = TryGetComponent<SceneLoader>(out var sceneLoader) ?
+            sceneLoader : gameObject.AddComponent<SceneLoader>();
     }
 }
